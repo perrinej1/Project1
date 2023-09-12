96acae6522c9cb21dcfa58d597ba6a318af968c6
perrinej1 <perrinej1@xavier.edu>
2023-09-11 20:44:39 -0400

    updating part5 with comments

--- part5.c
+++ part5.c
@@ -4,6 +4,8 @@
 
 int main(int argc, char **argv)
 {
+  //runs execv and the program in the arguments, and if the program dosn't
+  //exist, then it prints error and terminates.
   if(execv(argv[1], argv))
   {
     printf("execv did not work due to this: error %d %s\n",errno,strerror(errno));
